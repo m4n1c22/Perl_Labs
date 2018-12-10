a285d664a4673627ccf95668af1aafd976c372a6
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2018-12-10 17:45:30 +0100

    Update aufgabe6.pl

--- Praktikum-3/aufgabe6.pl
+++ Praktikum-3/aufgabe6.pl
@@ -2,13 +2,13 @@
 $limit = 100;
 $count=0;
 for($a=1;$a<$limit;$a++) {
-for($b=$a;$b<$limit;$b++) {
-$sq_c = $a*$a + $b*$b;
-$c = sqrt($sq_c);
-#regular expression to check if the value is an integer or not.
-if ($c =~ /^[+-]?\d+$/ ) {
-$count++;
-}
-}
+  for($b=$a;$b<$limit;$b++) {
+    $sq_c = $a*$a + $b*$b;
+    $c = sqrt($sq_c);
+    #regular expression to check if the value is an integer or not.
+    if ($c =~ /^[+-]?\d+$/ ) {
+      $count++;
+    }
+  }
 }
 print "Number of right triangles between side length less than 500 are: $count";
