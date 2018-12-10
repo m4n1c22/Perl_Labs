32798baab33eff4f5b9535d94b5a0cc14cc7e3ee
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2018-12-10 17:49:31 +0100

    Update aufgabe1.pl

--- Praktikum-3/aufgabe1.pl
+++ Praktikum-3/aufgabe1.pl
@@ -3,40 +3,39 @@ $a = <STDIN>;
 $b = <STDIN>;
 $c = <STDIN>;
 if(($a!=b)&&($b!=$c)&&($a!=$c)) {
-if($a>$b) {
-if($a>$c) {
-print "Largest value: $a";
+  if($a>$b) {
+    if($a>$c) {
+      print "Largest value: $a";
+    }
+    else {
+      print "Largest value: $c";
+    }
+  }
+  else {
+    if($b>$c) {
+      print "Largest value: $b";
+    }
+    else {
+      print "Largest value: $c";
+    }
+  }
+  if($a<$b) {
+    if($a<$c) {
+      print "Smallest value: $a";
+    }
+    else {
+      print "Smallest value: $c";
+    } 
+  }
+  else {
+    if($b<$c) {
+      print "Smallest value: $b";
+    }
+    else {
+      print "Smallest value: $c";
+    }
+  }
 }
 else {
-print "Largest value: $c";
-}
-}
-else {
-if($b>$c) {
-print "Largest value: $b";
-}
-else {
-print "Largest value: $c";
-}
-}
-
-if($a<$b) {
-if($a<$c) {
-print "Smallest value: $a";
-}
-else {
-print "Smallest value: $c";
-}
-}
-else {
-if($b<$c) {
-print "Smallest value: $b";
-}
-else {
-print "Smallest value: $c";
-}
-}
-}
-else {
-print "One of the passed values are same."
+  print "One of the passed values are same."
 }
