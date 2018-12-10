0b1a13b258c86ec427d41dc34c75b03887fcaf39
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2018-12-10 17:42:55 +0100

    Create aufgabe2_1stpart.pl

---
+++ Praktikum-3/aufgabe2_1stpart.pl
@@ -0,0 +1,13 @@
+#Program to determine a factorial of a number
+$n = <STDIN>;
+#initializing the value of fact variable as one.
+$fact = 1;
+#for loop construct used for computing the factorial of a number.
+# initially the value is set as 1 for fact variable and then
+# we decrementally multiply variable n with fact.
+# fact = fact * n where n is decremented from user defined value to 1.
+
+for(; $n>=1; $n--) {
+$fact = $fact*$n;
+}
+print "Factorial of n is $fact";
