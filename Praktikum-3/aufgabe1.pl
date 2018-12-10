8a5d6f18b7f95b981a42819a5c8478cf92bcfa8b
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2018-12-10 17:42:32 +0100

    Create aufgabe1.pl

---
+++ Praktikum-3/aufgabe1.pl
@@ -0,0 +1,42 @@
+# a,b,c finding the largest, smallest - Aufgabe 1_Ex3
+$a = <STDIN>;
+$b = <STDIN>;
+$c = <STDIN>;
+if(($a!=b)&&($b!=$c)&&($a!=$c)) {
+if($a>$b) {
+if($a>$c) {
+print "Largest value: $a";
+}
+else {
+print "Largest value: $c";
+}
+}
+else {
+if($b>$c) {
+print "Largest value: $b";
+}
+else {
+print "Largest value: $c";
+}
+}
+
+if($a<$b) {
+if($a<$c) {
+print "Smallest value: $a";
+}
+else {
+print "Smallest value: $c";
+}
+}
+else {
+if($b<$c) {
+print "Smallest value: $b";
+}
+else {
+print "Smallest value: $c";
+}
+}
+}
+else {
+print "One of the passed values are same."
+}
