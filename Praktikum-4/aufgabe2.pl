e27537ee80daa0c9f8cf39b35eaec4f0a8f41bee
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2018-12-10 17:34:27 +0100

    Incomplete frequency function yet to be added.

---
+++ Praktikum-4/aufgabe2.pl
@@ -0,0 +1,37 @@
+#Random number generator code aufgabe 2.1
+
+sub randomPositiveInteger {
+    
+    $num_args = scalar(@_);
+    if($num_args > 1) {
+        print "arguments are more";
+        exit;
+    }
+    $max_val = @_[0];
+    return int(rand($max_val)+1);
+}
+
+#Values of 10 sides of the cube are between 1-10
+sub rollD10 {
+    return randomPositiveInteger(10);
+}
+
+sub simulate_throws {
+    $num_args = scalar(@_);
+    if($num_args > 1) {
+        print "arguments are more";
+        exit;
+    }
+    $num_throws = @_[0];
+    
+    print "Result after $num_throws:\n";
+    for($i=0;$i<$num_throws;$i++) {
+        $result = rollD10();
+        print "$result\n";
+    }
+}
+
+simulate_throws(1000);
+
+
+
